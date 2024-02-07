a4f7888d19484f5df59444e58d4774cdf3f8817f
JayvonK <jaykhuth@gmail.com>
2024-02-06 20:03:27 -0800

    save

--- scripts/app.js
+++ scripts/app.js
@@ -4,3 +4,15 @@ let getUserBtn = document.getElementById("getUserBtn");
 let userInput = document.getElementById("userInput");
 let searchedUsers = document.getElementById("searchedUsers");
 
+let userArr = [];
+let count = 0;
+let run = false;
+
+const GetData = async () => {
+    const promise = await fetch('https://random-data-api.com/api/v2/users');
+    const data = await promise.json();
+    return data;
+}
+
+
+
