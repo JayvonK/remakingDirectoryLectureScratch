54a129a718ef3b71389814eaca30ee1ec6b0a356
JayvonK <jaykhuth@gmail.com>
2024-02-06 19:58:21 -0800

    js

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,6 @@
-alert("hello world");
+let userName = document.getElementById("userName");
+let btnDiv = document.getElementById("btnDiv");
+let getUserBtn = document.getElementById("getUserBtn");
+let userInput = document.getElementById("userInput");
+let searchedUsers = document.getElementById("searchedUsers");
+
