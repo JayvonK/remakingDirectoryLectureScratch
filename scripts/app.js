1317cf0cd68bebd0dfb913275af69d027633d62c
JayvonK <jaykhuth@gmail.com>
2024-02-06 20:27:10 -0800

    data arr change

--- scripts/app.js
+++ scripts/app.js
@@ -18,10 +18,42 @@ getUserBtn.addEventListener('click', async (event) => {
     if(run === false){
         for(let i = 0; i < 15; i++){
             let data = await GetData();
-            userArr.push(data.first_name + " " + data.last_name);
+            userArr.push(data);
         } 
-        userName.textContent = userArr[count];
+        userName.textContent = userArr[count].first_name + " " + userArr[count].last_name;
         userInput.readOnly = false;
+        CreatePrevBtn();
+        CreateNextBtn(); 
     }
+    run = true;
 })
+
+const CreateNextBtn = () => {
+    let button = document.createElement("button");
+    button.innerText = "Next";
+    button.addEventListener('click', (event) => {
+        if(count >= userArr.length - 1){
+            count = 0;
+        } else {
+           count++; 
+        }
+        userName.textContent = userArr[count].first_name + " " + userArr[count].last_name;
+    })
+    btnDiv.append(button);
+}
+
+const CreatePrevBtn = () => {
+    let button = document.createElement("button");
+    button.innerText = "Prev";
+    button.addEventListener('click', (event) => {
+        if(count <= 0){
+            count = userArr.length - 1;
+        } else {
+           count--; 
+        }
+        userName.textContent = userArr[count].first_name + " " + userArr[count].last_name;
+    })
+    btnDiv.append(button);
+}
+
 
